a4a62d1360b488e116a098163e93bbdeb622ac84
khaja shaik <khajashaik2626@gmail.com>
2026-02-09 10:55:58 +0530

    Add files via upload

---
+++ config-test.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = array(
+	'db'			=> 'mysql:host=localhost;dbname=hexagame',
+	'dbUser'		=> '',
+	'dbPassword'	=> '',
+
+	'googleId'		=> '',
+	'googleSecret'	=> '',
+	'googleRedirect'	=> '',
+
+	'salt'			=> ''		// This is used to salt the session hash, make it a complex string (alphanum+symbols)
+);
